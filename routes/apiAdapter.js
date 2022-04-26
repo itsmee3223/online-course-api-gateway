88e5b143b5b003f75b522d14fe4f23ad24fe1ba1
ramanda ajisaka <tamvam.pemberani@gmail.com>
2022-04-26 21:20:23 +0700

    fix typo

--- routes/apiAdapter.js
+++ routes/apiAdapter.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
+
 const { TIMEOUT } = process.env;
 
 module.exports = (baseUrl) => {
   return axios.create({
-    baseUrl: baseUrl,
-    timeout: TIMEOUT,
+    baseURL: baseUrl,
+    timeout: parseInt(TIMEOUT),
   });
 };
