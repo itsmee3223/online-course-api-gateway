e8510905d1a75b27ac0c14e7ad1f384730549803
ramanda ajisaka <tamvam.pemberani@gmail.com>
2022-04-26 21:24:12 +0700

    integrasi microservices media

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,9 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const mediaHandler = require("./handler/media");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.post("/", mediaHandler.create);
+router.get("/", mediaHandler.getAll);
+router.delete("/:id", mediaHandler.destroy);
 
 module.exports = router;
