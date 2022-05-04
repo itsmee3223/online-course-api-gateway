4aa6317f20a233e883bfbee8f2f36dc7ab63b4a4
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-04 21:28:17 +0700

    integrasi update & delete mentor

--- routes/mentors.js
+++ routes/mentors.js
@@ -6,5 +6,7 @@ const mentorHandler = require("./handler/mentors");
 router.post("/", mentorHandler.create);
 router.get("/", mentorHandler.getAll);
 router.get("/:id", mentorHandler.get);
+router.put("/:id", mentorHandler.update);
+router.delete("/:id", mentorHandler.destroy);
 
 module.exports = router;
