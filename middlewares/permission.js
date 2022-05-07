925294a044ad3050a10298e0b08c53449d4a4bd3
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-07 20:36:29 +0700

    middleware to verifiy permission

---
+++ middlewares/permission.js
@@ -0,0 +1,12 @@
+module.exports = (...roles) => {
+  return (req, res, next) => {
+    const { role } = req.user.data;
+    if (!roles.includes(role)) {
+      return res.status(405).json({
+        status: "error",
+        message: "you don't have permission to",
+      });
+    }
+    return next();
+  };
+};
