10bfd3b821edf3737ad9a6815c6958e7f7076862
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-07 19:52:14 +0700

    add router to app

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const mediaRouter = require("./routes/media");
 const coursesRouter = require("./routes/courses");
-const paymentsRouter = require("./routes/payments");
-const ordersRouter = require("./routes/orders");
+const paymentsRouter = require("./routes/orderPayments");
+const ordersRouter = require("./routes/orderPayments");
 const refreshTokenRouter = require("./routes/refreshTokens");
 const mentorsRouter = require("./routes/mentors");
 const chaptersRouter = require("./routes/chapters");
 const lessonsRouter = require("./routes/lessons");
 const imageCourseRouter = require("./routes/imageCourse");
 const myCourseRouter = require("./routes/myCourses");
+const webhookRouter = require("./routes/webhook");
 
 const verifyToken = require("./middlewares/verifyToken");
 
@@ -32,12 +33,13 @@ app.use("/users", usersRouter);
 app.use("/media", mediaRouter);
 app.use("/courses", coursesRouter);
 app.use("/payments", paymentsRouter);
-app.use("/orders", ordersRouter);
+app.use("/orders", verifyToken, ordersRouter);
 app.use("/refresh-tokens", refreshTokenRouter);
 app.use("/mentors", verifyToken, mentorsRouter);
 app.use("/chapters", verifyToken, chaptersRouter);
 app.use("/lessons", verifyToken, lessonsRouter);
 app.use("/image-courses", verifyToken, imageCourseRouter);
 app.use("/my-courses", verifyToken, myCourseRouter);
+app.use("/webhook", webhookRouter);
 
 module.exports = app;
