281cefe5cf69a4b0eae6b8ea4219f64f0d049e03
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-17 04:23:53 +0700

    .

--- routes/handler/courses/getAll.js
+++ routes/handler/courses/getAll.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   try {
     const courses = await api.get("/api/courses", {
       params: {
-        q: req.query,
+        ...req.query,
         status: "published",
       },
     });
