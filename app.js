a169b642db310f2dd2491bb3e3816673c915318f
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-16 20:57:18 +0700

    typo cors call function

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const can = require("./middlewares/permission");
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
